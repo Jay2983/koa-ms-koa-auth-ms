5dfe2b23f79c40268b3ee703d40647b7c2269ebb
Vamshree Siramdasu <vamshree@outlook.com>
2022-06-28 20:55:59 +1000

    add sinon stubs for get/:userid and post

--- db/queries/users.js
+++ db/queries/users.js
@@ -4,6 +4,20 @@ function getAllUsers() {
   return knex('users').select('*');
 }
 
+function getOneUser(userId) {
+  return knex('users')
+    .select('*')
+    .where('id', userId);
+}
+
+function createOne(user) {
+  return knex('users')
+    .insert(user)
+    .returning('*');
+}
+
 module.exports = {
   getAllUsers,
+  getOneUser,
+  createOne,
 };
